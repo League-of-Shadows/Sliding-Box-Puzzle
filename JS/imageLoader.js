371f333f3a39b507ca4624c5198daea5bfc9db5d
Aditya Yadav <BraveStone9>
2021-11-07 23:40:34 +0530

    Map feature and puzzle updates

--- JS/imageLoader.js
+++ JS/imageLoader.js
@@ -1,13 +1,12 @@
-var selectedImage = localStorage.getItem("src")
+var bounds = JSON.parse(localStorage.getItem("bounds"));
 
-console.log(selectedImage)
-console.log(document.getElementById('puzzleImage').src)
+src = "https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/"+bounds[0]+","+bounds[1]+","+bounds[2]+",0/500x400?access_token=pk.eyJ1IjoiYnJhdmVzdG9uZTkiLCJhIjoiY2tybHpxb2NvMGk0YTJwbm96eHFrZ3lhZCJ9.XC7Aw273nUmc9uzoKveEiA"
+
+console.log(bounds)
+console.log(src)
 
 window.addEventListener('load', () => {
 
-    document.getElementById('bgimage').src = selectedImage
-    document.getElementById('puzzleImage').src = selectedImage
+    document.getElementById('puzzleImage').src = src
 
 })
-
-console.log(document.getElementById('puzzleImage').src)
--- JS/location.js
+++ JS/location.js
@@ -1,20 +1,46 @@
-
-function imageSrc(){
-
-    selectedImage = document.getElementById("imageSelector").value
-    startPuzzle(selectedImage)
-
-}
-
-
-
-function startPuzzle(selectedImage){
-
-    console.log(selectedImage)
-
-    localStorage.setItem("src", selectedImage)
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYnJhdmVzdG9uZTkiLCJhIjoiY2tybHpxb2NvMGk0YTJwbm96eHFrZ3lhZCJ9.XC7Aw273nUmc9uzoKveEiA';
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/satellite-v9', // style URL
+        center: [-122.25948, 37.87221], // starting position
+        zoom: 15 // starting zoom
+    });
     
-    window.open("puzzle.html")
+    // Add zoom and rotation controls to the map.
+    // map.addControl(new mapboxgl.NavigationControl());
 
-}
 
+    const geocoder = new MapboxGeocoder({
+        // Initialize the geocoder
+        accessToken: mapboxgl.accessToken, // Set the access token
+        mapboxgl: mapboxgl, // Set the mapbox-gl instance
+        marker: false // Do not use the default marker style
+    });
+
+    // Add the geocoder to the map
+    map.addControl(geocoder, "top-left");
+
+    var nav = new mapboxgl.NavigationControl({
+        showCompass: true,
+        showZoom: true,
+        visualizePitch: true
+    });
+
+    map.addControl(nav, "top-right");
+
+    function captureFrame(){
+
+        var bounds = []
+        bounds[0] = map.getCenter().lng
+        bounds[1] = map.getCenter().lat
+        bounds[2] = map.getZoom()
+
+        console.log("bounds",bounds)
+
+        startPuzzle(bounds)
+    }
+
+    function startPuzzle(bounds){
+        localStorage.setItem("bounds", JSON.stringify(bounds))
+        window.open("puzzle.html")
+    }
