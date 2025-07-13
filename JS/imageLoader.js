b15e378ede726f0c3d927959eea22256ed13b11e
aditya yadav <aditya27yadav@gmail.com>
2025-07-13 15:28:21 +0200

    Fix font family typos and declare src

--- JS/imageLoader.js
+++ JS/imageLoader.js
@@ -1,6 +1,6 @@
 var bounds = JSON.parse(localStorage.getItem("bounds"));
 
-src = "https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/"+bounds[0]+","+bounds[1]+","+bounds[2]+",0/500x400?access_token=pk.eyJ1IjoiYnJhdmVzdG9uZTkiLCJhIjoiY2tybHpxb2NvMGk0YTJwbm96eHFrZ3lhZCJ9.XC7Aw273nUmc9uzoKveEiA"
+const src = "https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/"+bounds[0]+","+bounds[1]+","+bounds[2]+",0/500x400?access_token=pk.eyJ1IjoiYnJhdmVzdG9uZTkiLCJhIjoiY2tybHpxb2NvMGk0YTJwbm96eHFrZ3lhZCJ9.XC7Aw273nUmc9uzoKveEiA";
 
 console.log(bounds)
 console.log(src)
